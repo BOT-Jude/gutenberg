b166f1449e3e592b5af03358195ce5d8bfde6b25
BOT-Jude <Jude@Puddicombe.com>
2024-12-27 10:34:09 +0000

    using huggingface instead, leaving this for now

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
-python -m venv ./venv
-./venv/Scripts/Activate.ps1
-pip install torch
-pip install pandas
-pip install gutenberg
-pip install requests
-python load.py
+pyenv install 3.8.10
+pyenv global 3.8.10
+
+
+#python -m venv ./venv
+#./venv/Scripts/activate
+#pip install torch
+#pip install pandas
+#pip install gutenberg
+#pip install requests
+#pip install beautifulsoup4
+#python load.py
